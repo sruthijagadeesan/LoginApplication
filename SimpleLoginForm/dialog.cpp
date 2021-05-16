cd39be7e1b5b34a5e9926a212d42a8136f2fc08e
Sruthi Jagadeesan <sruthi3.jagadeesan@gmail.com>
2021-05-16 20:40:42 +0530

    16.05.2021

---
+++ SimpleLoginForm/dialog.cpp
@@ -0,0 +1,40 @@
+#include "dialog.h"
+#include "ui_dialog.h"
+#include <QDebug>
+
+Dialog::Dialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::Dialog)
+{
+    ui->setupUi(this);
+}
+
+Dialog::~Dialog()
+{
+    delete ui;
+}
+
+void Dialog::on_exit_clicked()
+{
+    close();
+}
+
+void Dialog::on_confirm_clicked()
+{
+    QString email = ui->lineEdit->text();
+        QString pwd = ui->lineEdit_2->text();
+        QString confirmPwd = ui->lineEdit_3->text();
+        qDebug() << "Email :" << email;
+        qDebug() << "Password :" << pwd;
+        qDebug() << "Confirm Password :" << confirmPwd;
+
+        if(!email.isEmpty() && !pwd.isEmpty() && !confirmPwd.isEmpty())
+        {
+
+        }
+        else
+        {
+            qDebug() << "One Field is Empty";
+        }
+    }
+
